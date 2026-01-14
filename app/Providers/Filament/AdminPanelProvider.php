a9d51e4f1732b4b847ebdfc87750730fb747ccea
João <joaoljolival@gmail.com>
2026-01-14 11:03:46 +0000

    chore: setup pint, phpstan (level 6), and in-memory sqlite for local testing

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -9,7 +9,6 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Joaoolival\LaravelBlogEngine\BlogPlugin;
 use Filament\Support\Colors\Color;
 use Filament\Widgets\AccountWidget;
 use Filament\Widgets\FilamentInfoWidget;
@@ -19,6 +18,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Joaoolival\LaravelBlogEngine\BlogPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
